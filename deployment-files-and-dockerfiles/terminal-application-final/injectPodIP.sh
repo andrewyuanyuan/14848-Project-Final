53d725fe0fb96c66adeba5894469bf839da4d0e2
Anyuan Yu <andrewyuanyuan@163.com>
2021-11-29 22:20:54 -0800

    update

--- deployment-files-and-dockerfiles/terminal-application-final/injectPodIP.sh
+++ deployment-files-and-dockerfiles/terminal-application-final/injectPodIP.sh
@@ -1,9 +1,7 @@
 #!bin/bash
 
 NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="ExternalIP")].address }')
-search="MY_NODE_IP"
-filename="./terminal-application-deployment.yaml"
-sed -i "s/$search/$NODE_IP/" $filename
+sed -i "s/MY_NODE_IP/$NODE_IP/" ./terminal-application-deployment.yaml
 
 kubectl apply -f ./terminal-application-deployment.yaml
 kubectl apply -f ./service-terminal-application-lb.yaml
